5f881b7af8ba663ebc35bae3d795519d76679f21
xujunwei <825729891@qq.com>
2017-01-16 10:29:53 +0800

    swfupload示例

---
+++ src/com/model/Picture.java
@@ -0,0 +1,18 @@
+package com.model;
+
+public class Picture {
+
+	private String pic_name;
+	private String pic_message;
+	private String pic_path;
+	public String getPic_name() {
+		return pic_name;
+	}
+	public String getPic_message() {
+		return pic_message;
+	}
+	public String getPic_path() {
+		return pic_path;
+	}
+	
+}
